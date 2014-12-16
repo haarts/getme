7544075d668ac5915725832d409350dad2aa87b7
Harm Aarts <harmaarts@gmail.com>
2014-12-16 23:17:28 +0000

    Extracts search into seperate package.
    All that remains now is to define a generic interface.

--- getme.go
+++ getme.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"os"
-	"sort"
+
+	"github.com/haarts/getme/providers"
 )
 
 func getQuery() string {
@@ -19,49 +17,8 @@ func getQuery() string {
 	return query
 }
 
-//TODO move this to a (search) providers package. Make sure the return type implements a certain interface.
-type ratings struct {
-	Votes int `json:"votes"`
-}
-
-type match struct {
-	Title   string  `json:"title"`
-	Ratings ratings `json:"ratings"`
-}
-
-type byRating []match
-
-func (a byRating) Len() int           { return len(a) }
-func (a byRating) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a byRating) Less(i, j int) bool { return a[i].Ratings.Votes > a[j].Ratings.Votes }
-
-func search(query string) []match {
-	resp, err := http.Get("http://api.trakt.tv/search/shows.json/5bc6254d3bbde304a49557cf2845d921?query=" + query)
-	if err != nil { //TODO also error out on anything but a 200 Response
-		fmt.Println("Error when searching: ", err)
-		os.Exit(1) //TODO retry a couple of times when it's a timeout.
-	}
-	defer resp.Body.Close()
-
-	fmt.Printf("resp %+v\n", resp)
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Error when parsing response: ", err)
-		os.Exit(1)
-	}
-
-	var matches []match
-	err = json.Unmarshal(body, &matches)
-	if err != nil {
-		fmt.Println("Error unmarshaling response: ", err)
-	}
-
-	sort.Sort(byRating(matches))
-	return matches
-}
-
 func main() {
 	query := getQuery()
-	matches := search(query)
+	matches := providers.Search(query)
 	fmt.Printf("matches %+v\n", matches)
 }
---
+++ providers/trakt.go
@@ -0,0 +1,49 @@
+package providers
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"sort"
+)
+
+type ratings struct {
+	Votes int `json:"votes"`
+}
+
+type Match struct {
+	Title   string  `json:"title"`
+	Ratings ratings `json:"ratings"`
+}
+
+type byRating []Match
+
+func (a byRating) Len() int           { return len(a) }
+func (a byRating) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a byRating) Less(i, j int) bool { return a[i].Ratings.Votes > a[j].Ratings.Votes }
+
+func Search(query string) []Match {
+	resp, err := http.Get("http://api.trakt.tv/search/shows.json/5bc6254d3bbde304a49557cf2845d921?query=" + query)
+	if err != nil { //TODO also error out on anything but a 200 Response
+		fmt.Println("Error when searching: ", err)
+		os.Exit(1) //TODO retry a couple of times when it's a timeout.
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error when parsing response: ", err)
+		os.Exit(1)
+	}
+
+	var matches []Match
+	err = json.Unmarshal(body, &matches)
+	if err != nil {
+		fmt.Println("Error unmarshaling response: ", err)
+	}
+
+	sort.Sort(byRating(matches))
+	return matches
+}
