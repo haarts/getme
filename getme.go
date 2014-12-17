e09b08649c997cc407ba9e1608958b9f63017682
Harm Aarts <harmaarts@gmail.com>
2014-12-17 17:04:28 +0000

    Sketch the interface.

--- getme.go
+++ getme.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 
@@ -17,12 +18,42 @@ func getQuery() string {
 	return query
 }
 
+func showBestMatch(bestMatch sources.Match) {
+	fmt.Println("The best match we found is:")
+	fmt.Println(" ", bestMatch.Title())
+}
+
+func askConfirmation() bool {
+	fmt.Print("Is this the one you want? [Y/n]")
+	bio := bufio.NewReader(os.Stdin)
+	line, err := bio.ReadString('\n')
+	if err != nil {
+		fmt.Printf("err %+v\n", err)
+	}
+
+	if line == "\n" || line == "y\n" || line == "Y\n" {
+		return true
+	} else {
+		return false
+	}
+}
+
 func main() {
 	query := getQuery()
 	matches, err := sources.Search(query)
 	if err != nil {
 		fmt.Printf("err %+v\n", err)
 	}
+
+	showBestMatch(matches.BestMatch())
+	bestMatchConfirmed := askConfirmation()
+	fmt.Printf("bestMatchConfirmed %+v\n", bestMatchConfirmed)
+	if bestMatchConfirmed {
+		// Store it somewhere.
+	} else {
+		// Show entire list and store selection.
+	}
+
 	fmt.Printf("matches %+v\n", matches)
 	fmt.Printf("matches.BestMatch() %+v\n", matches.BestMatch())
 }
