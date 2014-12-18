a452d3c5e791868e46e6fd437a9b8adc10653b8a
Harm Aarts <harmaarts@gmail.com>
2014-12-18 17:22:12 +0000

    Adds the plan for persisting store.

--- store/store.go
+++ store/store.go
@@ -10,11 +10,17 @@ type Store struct {
 	shows map[string]Show
 }
 
+// TODO deserialize from a bunch of files.
 func Open() *Store {
 	return &Store{
 		shows: make(map[string]Show),
 	}
 }
+
+// TODO adds serialization to a bunch of JSON files.
+// Plan: each show is a dir in shows/. Each seasons is a dir in that. And each
+// episode is a file in that. When an episode has been found and downloaded
+// just rename the file. The file contains some meta data.
 
 func (s *Store) CreateShow(m sources.Match) *Show {
 	show := Show{m.Title}
