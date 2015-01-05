c2a08d0361cd72706b918cf0b3c21b5900debc1b
Harm Aarts <harmaarts@gmail.com>
2015-01-05 16:28:52 +0000

    Fixes GoLint issues.

--- sources/sources.go
+++ sources/sources.go
@@ -1,25 +1,31 @@
+// Package sources provides the ability to search for shows/movies given a user
+// provide search string.
 package sources
 
 import (
-	"errors"
 	"fmt"
 	"regexp"
 	"time"
 )
 
+// Source defines the methods a new source for media info should implement.
 type Source interface {
 	Search(string) ([]Match, error)
 	AllSeasonsAndEpisodes(Show) ([]*Season, error)
 }
 
+// Match is either a Movie or a Show.
 type Match interface {
 	DisplayTitle() string
 }
 
+// Movie contains all the relevant information for a movie.
 type Movie struct {
 	Title string
 }
 
+// Show contains all the relevant information for a TV show. A value is Show is
+// the main way on interfacing with the show, seasons AND episodes.
 type Show struct {
 	Title      string    `json:"title"`
 	URL        string    `json:"url"`
@@ -29,12 +35,14 @@ type Show struct {
 	IsDaily    bool      `json:"is_daily"`
 }
 
+// Season is _always_ part of a Show and contains meta data on a season in the show.
 type Season struct {
 	Show     *Show      `json:"-"`
 	Season   int        `json:"season"`
 	Episodes []*Episode `json:"episodes"`
 }
 
+// Episode is _always_ part of a Season and contains meta data on an episode in the show.
 // TODO use TriedAt and Backoff to slowly stop trying to download episodes which prop never complete.
 type Episode struct {
 	Season  *Season   `json:"-"`
@@ -46,6 +54,8 @@ type Episode struct {
 	//Backoff int
 }
 
+// PendingItem is a generalisation of something which hasn't been downloaded
+// yet. This can be a single episode OR an entire season.
 type PendingItem struct {
 	//NOTE Perhaps we could return a set of fields in the future and let the search
 	//engine construct the string they want,
@@ -56,6 +66,8 @@ type PendingItem struct {
 
 var sources = make(map[string]Source)
 
+// Register should be called (in an init function) when adding a new source.
+// See trakt.go for an example.
 func Register(name string, source Source) {
 	if _, dup := sources[name]; dup {
 		panic("source: Register called twice for source " + name)
@@ -63,13 +75,16 @@ func Register(name string, source Source) {
 	sources[name] = source
 }
 
+// ListSources exists mainly for display purposes.
 func ListSources() (names []string) {
-	for name, _ := range sources {
+	for name := range sources {
 		names = append(names, name)
 	}
 	return
 }
 
+// Search is the important function of this package. Call this to turn a user
+// search string into a list of matches (which might be TV shows or movies).
 func Search(q string) ([][]Match, []error) {
 	var matches = make([][]Match, len(sources))
 	var errors = make([]error, len(sources))
@@ -83,7 +98,10 @@ func Search(q string) ([][]Match, []error) {
 	return matches, errors
 }
 
-// Initial adding of episodes.
+// GetSeasonsAndEpisodes should be called with a newly initialized Show. In
+// addition to fetching the seasons and episodes this determines if the show is
+// a daily show, which impacts the way queries are generated for search
+// engines.
 func GetSeasonsAndEpisodes(s *Show) error {
 	err := UpdateSeasonsAndEpisodes(s)
 	if err != nil {
@@ -94,10 +112,11 @@ func GetSeasonsAndEpisodes(s *Show) error {
 	return nil
 }
 
-// Subsequent updates of episodes
+// UpdateSeasonsAndEpisodes should be called to update a Show after, for
+// example, deserialization for disk.
 func UpdateSeasonsAndEpisodes(s *Show) error {
 	if _, ok := sources[s.SourceName]; !ok {
-		return errors.New(fmt.Sprintf("Source defined by show(%s) is not registered.", s.SourceName))
+		return fmt.Errorf("Source defined by show(%s) is not registered.", s.SourceName)
 	}
 
 	source := sources[s.SourceName]
@@ -156,6 +175,8 @@ func findExistingSeason(existing []*Season, other *Season) *Season {
 	return nil
 }
 
+// DisplayTitle returns the title of a movie. Here to satisfy the Match
+// interface.
 func (m Movie) DisplayTitle() string {
 	return m.Title
 }
@@ -169,10 +190,13 @@ func (s *Show) String() string {
 	)
 }
 
+// DisplayTitle returns the title of a TV show. Here to satisfy the Match
+// interface.
 func (s Show) DisplayTitle() string {
 	return s.Title
 }
 
+// Episodes lists all the episodes associated with this show.
 func (s Show) Episodes() (episodes []*Episode) {
 	for _, season := range s.Seasons {
 		episodes = append(episodes, season.Episodes...)
@@ -180,24 +204,26 @@ func (s Show) Episodes() (episodes []*Episode) {
 	return
 }
 
+// Done flags an episode as 'downloaded' and thus done. This episode is
+// never looked up on a search engine agian.
 func (p *PendingItem) Done() {
 	for _, episode := range p.episodes {
 		episode.Pending = false
 	}
 }
 
-// TODO cut this method up.
 // PendingItems returns a list of items which are to be downloaded.
 // The list consists of a mix of episodes and seasons.
 // A season is included when it is NOT the last season of the Show and when all containing episodes
 // are pending and it is NOT a daily show.
-func (show Show) PendingItems() (pendingItems []PendingItem) {
-	for _, season := range show.Seasons {
-		if !show.IsDaily && !show.isLastSeason(season) && season.allEpisodesPending() {
+// TODO cut this method up.
+func (s Show) PendingItems() (pendingItems []PendingItem) {
+	for _, season := range s.Seasons {
+		if !s.IsDaily && !s.isLastSeason(season) && season.allEpisodesPending() {
 			item := PendingItem{
-				QueryNames: []string{fmt.Sprintf("%s season %d", show.Title, season.Season)},
+				QueryNames: []string{fmt.Sprintf("%s season %d", s.Title, season.Season)},
 				episodes:   season.Episodes,
-				ShowTitle:  show.Title,
+				ShowTitle:  s.Title,
 			}
 			pendingItems = append(pendingItems, item)
 		} else {
@@ -205,14 +231,14 @@ func (show Show) PendingItems() (pendingItems []PendingItem) {
 			for _, episode := range episodes {
 				item := PendingItem{
 					episodes:  []*Episode{episode},
-					ShowTitle: show.Title,
+					ShowTitle: s.Title,
 				}
-				if show.IsDaily {
+				if s.IsDaily {
 					y, m, d := episode.AirDate.Date()
-					item.QueryNames = []string{fmt.Sprintf("%s %d %d %02d", show.Title, y, m, d)}
+					item.QueryNames = []string{fmt.Sprintf("%s %d %d %02d", s.Title, y, m, d)}
 				} else {
-					s1 := fmt.Sprintf("%s S%02dE%02d", show.Title, season.Season, episode.Episode)
-					s2 := fmt.Sprintf("%s %dx%d", show.Title, season.Season, episode.Episode)
+					s1 := fmt.Sprintf("%s S%02dE%02d", s.Title, season.Season, episode.Episode)
+					s2 := fmt.Sprintf("%s %dx%d", s.Title, season.Season, episode.Episode)
 					item.QueryNames = []string{s1, s2}
 				}
 				pendingItems = append(pendingItems, item)
@@ -222,6 +248,8 @@ func (show Show) PendingItems() (pendingItems []PendingItem) {
 	return
 }
 
+// PendingEpisodes returns all the episodes of this show which are still
+// pending for download.
 func (season Season) PendingEpisodes() (episodes []*Episode) {
 	for _, e := range season.Episodes {
 		if e.Pending {
@@ -231,19 +259,8 @@ func (season Season) PendingEpisodes() (episodes []*Episode) {
 	return
 }
 
-// TODO deprecated
-func (s Show) PendingEpisodes() (episodes []*Episode) {
-	allEpisodes := s.Episodes()
-	for _, e := range allEpisodes {
-		if e.Pending {
-			episodes = append(episodes, e)
-		}
-	}
-	return
-}
-
-func (s *Season) allEpisodesPending() bool {
-	for _, e := range s.Episodes {
+func (season *Season) allEpisodesPending() bool {
+	for _, e := range season.Episodes {
 		if !e.Pending {
 			return false
 		}
@@ -252,20 +269,16 @@ func (s *Season) allEpisodesPending() bool {
 }
 
 // NOTE Can't assume ordering.
-func (show Show) isLastSeason(season *Season) bool {
-	for _, s := range show.Seasons {
-		if s.Season > season.Season {
+func (s Show) isLastSeason(currentSeason *Season) bool {
+	for _, season := range show.Seasons {
+		if season.Season > currentSeason.Season {
 			return false
 		}
 	}
 	return true
 }
 
-// TODO deprecated?
-func (e *Episode) ShowName() string {
-	return e.Season.Show.Title
-}
-
+// AsFileName turns an episode into a filesystem save filename.
 // TODO deprecated?
 func (e *Episode) AsFileName() string {
 	re := regexp.MustCompile("[^a-zA-Z0-9]")
@@ -298,14 +311,15 @@ func isNextDay(d1, d2 time.Time) bool {
 	return d.Hours()/24 == -1
 }
 
+// QueryNames is probably deprecated.
 // TODO deprecated
 func (e *Episode) QueryNames() []string {
 	if e.Season.Show.IsDaily { // Potential train wreck
 		y, m, d := e.AirDate.Date()
 		return []string{fmt.Sprintf("%s %d %d %d", e.ShowName(), y, m, d)}
-	} else {
-		s1 := fmt.Sprintf("%s S%02dE%02d", e.ShowName(), e.Season.Season, e.Episode)
-		s2 := fmt.Sprintf("%s %dx%d", e.ShowName(), e.Season.Season, e.Episode)
-		return []string{s1, s2}
 	}
+
+	s1 := fmt.Sprintf("%s S%02dE%02d", e.ShowName(), e.Season.Season, e.Episode)
+	s2 := fmt.Sprintf("%s %dx%d", e.ShowName(), e.Season.Season, e.Episode)
+	return []string{s1, s2}
 }
