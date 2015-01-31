8d4f5ba78374eb08c3503c943639dc715478365c
Harm Aarts <harmaarts@gmail.com>
2015-01-31 09:25:36 +0100

    Consolidate Trakt files in one place.
    I've renamed a couple of structs to better reflect the actual return
    types. These are the first steps toward making a Go Trakt client.

--- sources/trakt.go
+++ sources/trakt.go
@@ -1,9 +1,12 @@
+// TODO this should just be a Trakt client. Minorring the API endpoints from Trakt.
 package sources
 
 import (
+	"fmt"
 	"net/http"
 	"net/url"
 	"sort"
+	"time"
 
 	"github.com/haarts/getme/store"
 )
@@ -18,7 +21,22 @@ func init() {
 	Register(traktName, Trakt{})
 }
 
-type traktMatch struct {
+type traktSeason struct {
+	Season   int `json:"number"`
+	Episodes int `json:"episode_count"`
+}
+
+type traktEpisode struct {
+	Number     int        `json:"number"`
+	Title      string     `json:"title"`
+	FirstAired *time.Time `json:"first_aired"`
+}
+
+type traktShow struct {
+	// WHat to do what to do?
+}
+
+type traktSearchResult struct {
 	Score float64 `json:"score"`
 	Show  struct {
 		Title string `json:"title"`
@@ -28,7 +46,7 @@ type traktMatch struct {
 	} `json:"show"`
 }
 
-type byScore []traktMatch
+type byScore []traktSearchResult
 
 func (a byScore) Len() int           { return len(a) }
 func (a byScore) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
@@ -40,7 +58,15 @@ var clientID = "01045164ed603042b53acf841b590f0e7b728dbff319c8d128f8649e2427cbe9
 var traktURL = "https://api.trakt.tv"
 var traktSearchURL = traktURL + "/search?type=show"
 
-func constructURL(query string) string {
+func (t Trakt) seasonURL(show store.Show, season store.Season) string {
+	return fmt.Sprintf(traktURL+"/shows/%s/seasons/%d?extended=full", show.URL, season.Season)
+}
+
+func (t Trakt) seasonsURL(show store.Show) string {
+	return fmt.Sprintf(traktURL+"/shows/%s/seasons?extended=full", show.URL)
+}
+
+func searchShowURL(query string) string {
 	escapedQuery := url.Values{}
 	escapedQuery.Add("query", query)
 	return traktSearchURL + "&" + escapedQuery.Encode()
@@ -61,12 +87,12 @@ func traktRequest(URL string) (*http.Request, error) {
 
 // Search returns matches found by this source based on the query.
 func (t Trakt) Search(query string) ([]Match, error) {
-	req, err := traktRequest(constructURL(query))
+	req, err := traktRequest(searchShowURL(query))
 	if err != nil {
 		return nil, err
 	}
 
-	ms := &([]traktMatch{})
+	ms := &([]traktSearchResult{})
 
 	err = GetJSON(req, ms)
 	if err != nil {
@@ -78,7 +104,9 @@ func (t Trakt) Search(query string) ([]Match, error) {
 	return convertToMatches(*ms), nil
 }
 
-func convertToMatches(ms []traktMatch) []Match {
+// TODO expand Matches construct to incorporate a Show. Hell, let's not return
+// a Match here but a TraktShow.
+func convertToMatches(ms []traktSearchResult) []Match {
 	matches := make([]Match, len(ms))
 	for i, m := range ms {
 		matches[i] = store.Show{
@@ -88,4 +116,71 @@ func convertToMatches(ms []traktMatch) []Match {
 		}
 	}
 	return matches
+}
+
+// AllSeasonsAndEpisodes finds the seasons and episodes for a show with this source.
+// TODO get rid of anything store related. Return own types and let the caller
+// decide what to do with it.
+func (t Trakt) AllSeasonsAndEpisodes(show store.Show) ([]*store.Season, error) {
+	req, err := traktRequest(t.seasonsURL(show))
+	if err != nil {
+		return nil, err
+	}
+
+	ss := &([]traktSeason{})
+
+	err = GetJSON(req, ss)
+	if err != nil {
+		return nil, err
+	}
+
+	seasons := convertToSeasons(*ss)
+	err = t.addEpisodes(seasons, show)
+	if err != nil {
+		return nil, err
+	}
+	return seasons, nil
+}
+
+// TODO Quite a bit of duplication with the convertToMatches function.
+func convertToSeasons(ss []traktSeason) []*store.Season {
+	seasons := make([]*store.Season, 0, len(ss))
+	for _, s := range ss {
+		season := &store.Season{
+			Season:   s.Season,
+			Episodes: make([]*store.Episode, 0, s.Episodes),
+		}
+		seasons = append(seasons, season)
+	}
+	return seasons
+}
+
+func (t Trakt) addEpisodes(seasons []*store.Season, show store.Show) error {
+	for _, season := range seasons {
+		req, err := traktRequest(t.seasonURL(show, *season))
+		if err != nil {
+			return err
+		}
+
+		episodes := &([]traktEpisode{})
+		err = GetJSON(req, episodes)
+		if err != nil {
+			return err
+		}
+
+		for _, episode := range *episodes {
+			if episode.FirstAired == nil {
+				episode.FirstAired = &time.Time{}
+			}
+			e := store.Episode{
+				Title:   episode.Title,
+				Episode: episode.Number,
+				Pending: true, // NOTE Do not forget to set pending to true!
+				AirDate: *episode.FirstAired,
+			}
+
+			season.Episodes = append(season.Episodes, &e)
+		}
+	}
+	return nil
 }
--- sources/trakt_test.go
+++ sources/trakt_test.go
@@ -7,6 +7,8 @@ import (
 	"net/http/httptest"
 	"os"
 	"testing"
+
+	"github.com/haarts/getme/store"
 )
 
 func readFixture(file string) string {
@@ -37,5 +39,53 @@ func TestOrdering(t *testing.T) {
 	matches := Search("some query")
 	if matches[0].Matches[0].DisplayTitle() != "Game of Thrones" {
 		t.Fatal("best match is not Game of Thrones, got:", matches[0].Matches[0].DisplayTitle())
+	}
+}
+
+func TestExpandShow(t *testing.T) {
+	stack := []string{
+		"testdata/trakt/seasons.json",
+		"testdata/trakt/season_0.json",
+		"testdata/trakt/season_1.json",
+		"testdata/trakt/season_2.json",
+		"testdata/trakt/season_3.json",
+		"testdata/trakt/season_4.json",
+		"testdata/trakt/season_5.json",
+	}
+
+	var f string
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		f, stack = stack[0], stack[1:len(stack)] // *POP*
+		fmt.Fprintln(w, readFixture(f))
+	}))
+	defer ts.Close()
+
+	traktURL = ts.URL
+
+	show := &store.Show{URL: "boo/some-url", SourceName: traktName, Title: "Awesome"}
+	err := GetSeasonsAndEpisodes(show)
+	if err != nil {
+		t.Fatal("Expected not an error, got:", err)
+	}
+	if len(show.Seasons) != 6 {
+		t.Fatal("Expected 6 seasons, got:", len(show.Seasons))
+	}
+
+	if show.Seasons[1].Season == 0 {
+		t.Error("Expected Season field to be not default, got:", show.Seasons[1])
+	}
+
+	season := show.Seasons[0]
+	if len(season.Episodes) != 10 {
+		t.Fatal("Expected 10 episodes, got:", len(show.Seasons[0].Episodes))
+	}
+
+	episode := season.Episodes[0]
+	if episode.Episode != 1 {
+		t.Error(
+			"Expected episode number to equal 10, got:",
+			episode.Episode,
+		)
 	}
 }
